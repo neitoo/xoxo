7df30f36df1a5a45e7809c30b66f2e8ffb4bb0d8
neitoo <samuraa855@gmail.com>
2024-03-27 14:05:03 +0500

    Исправлен грид

--- src/pages/PlayArea.jsx
+++ src/pages/PlayArea.jsx
@@ -1,34 +1,37 @@
-import React, {Component} from 'react';
-import '../styles/PlayArea.scss';
-import zeroImg from '../assets/zero.svg';
-import unionImg from '../assets/union.svg';
-import Board from '../components/Board';
+import React, { Component } from "react";
+import "../styles/PlayArea.scss";
+import zeroImg from "../assets/zero.svg";
+import unionImg from "../assets/union.svg";
+import Board from "../components/Board";
+import { PlayersAccordion } from "../components/PlayersAccordion";
 
-export default class PlayArea extends Component{
-    render(){
-        return(
-            <div className='wrap-container'>
+let playerList = [
+    {
+        side: "x",
+        fullname: "Прохоренков Никита Валерьевич",
+    },
+];
 
-                <div className='container'>
-                    <aside className='players'>
-
-                    </aside>
-                    <main className='container-board'>
-                        <div className='timer'>
-                            <p>6:00</p>
-                        </div>
-                        <Board/>
-                    </main>
-                    <aside className='chat'>
-
-                    </aside>
-                </div>
-                <div className='walks-player'>
+export default class PlayArea extends Component {
+    render() {
+        return (
+            <div className="wrap-container">
+                <aside className="players-container">
+                    <PlayersAccordion players={playerList} />
+                </aside>
+                <main className="container-board">
+                    <div className="timer">
+                        <p>6:00</p>
+                    </div>
+                    <Board />
+                </main>
+                <aside className="chat"></aside>
+                <div className="walks-player">
                     <p>Ходит</p>
-                    <img src={zeroImg}></img> 
+                    <img src={zeroImg} alt="Сторона игрока"></img>
                     <p>игрок</p>
                 </div>
             </div>
-        )
+        );
     }
 }
