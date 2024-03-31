c98663fc91440eb57db303298f00605879c36cce
neitoo <samuraa855@gmail.com>
2024-03-31 15:04:19 +0500

    Изменена обертка App

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/reset.css';
-import { BrowserRouter } from 'react-router-dom';
+import AuthProvider from './context/AuthC';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <AuthProvider>
       <App />
-    </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
