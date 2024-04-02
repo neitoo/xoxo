803fc1cb19165c0b965748791707735fb5eb0978
neitoo <samuraa855@gmail.com>
2024-04-02 17:25:07 +0500

    Реализована примитивная авторизация с тестовыми логином и паролем

--- src/context/AuthC.jsx
+++ src/context/AuthC.jsx
@@ -1,25 +1,57 @@
 import { createContext, useState, useEffect } from "react";
+import { signInYup } from "./validation";
+import { ColorRing } from "react-loader-spinner";
 
 export const AuthC = createContext({});
 
 const AuthProvider = ({children}) => {
     
     const [userData, setUserData] = useState({});
-    const [isAppReady, setIsAppReady] = useState(true);
+    const [isAppReady, setIsAppReady] = useState(false);
     const [isUserLogged, setIsUserLogged] = useState(false);
+    const [error, setError] = useState({});
 
-    const handleLogin = (username,password) => {
-        const testUserData = {
-            username: "pizzadabdab",
-            password: "piZZa1234_",
-            fullname: "Авторизированный игрок",
-            procentWin: 0,
+    const testUserData = {
+        login: "pizzadabdab",
+        password: "piZZa1234_",
+        fullname: "Авторизированный игрок",
+        procentWin: 0,
+    }
+
+    const handleSignIn = (login,password) => {
+        const isValidLogin = login === testUserData.login;
+        const isValidPassword = password === testUserData.password;
+        
+        const yupErrors = validateYupFields({ login, password });
+        
+        setError({
+            login: isValidLogin ? "" : "Неверный логин",
+            password: isValidPassword ? "" : "Неверный пароль",
+            ...yupErrors
+        });
+    
+        setIsUserLogged(isValidLogin && isValidPassword && Object.keys(yupErrors).length === 0);
+
+    }
+
+    const validateYupFields = (data) => {
+        try {
+            signInYup.validateSync(data, { abortEarly: false });
+            return {}; // Если валидация прошла успешно, возвращаем пустой объект ошибок
+        } catch (error) {
+            return error.inner.reduce((acc, curr) => {
+                acc[curr.path] = curr.message; // Формируем объект ошибок
+                return acc;
+            }, {});
         }
-
-        setIsUserLogged(true);
     }
 
     useEffect(() => {
+        setIsAppReady(false);
+            setTimeout(()=>{
+                setIsAppReady(true)
+        },1000);
+        
         if (isUserLogged) {
             setIsAppReady(true);
         }
@@ -29,12 +61,21 @@ const AuthProvider = ({children}) => {
         <AuthC.Provider
             value={{
                 isUserLogged,
-                handleLogin
+                handleSignIn,
+                error
             }}
         >
             {isAppReady ? (children) : (
-                <div>
-                    Загрузка...
+                <div className="login-wrapper">
+                    <ColorRing
+                        visible={true}
+                        height="80"
+                        width="80"
+                        ariaLabel="color-ring-loading"
+                        wrapperStyle={{}}
+                        wrapperClass="color-ring-wrapper"
+                        colors={['#EB0057', '#60C2AA', '#EB0057', '#60C2AA', '#EB0057']}
+                    />
                 </div>
             )}
         </AuthC.Provider>
