1e4465cf9dd29e97124be15a3844631c5decfbd8
neitoo <samuraa855@gmail.com>
2024-04-02 17:24:44 +0500

    Реализована авторизация с проверкой полей и отображением  ошибок

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -5,23 +5,65 @@ import "../styles/SignIn.scss"
 import { useForm } from "react-hook-form";
 import { AuthC } from "../context/AuthC";
 import { useContext } from "react";
-import { Navigate } from "react-router-dom";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { signInYup } from "../context/validation";
+
+const defaultValues = {
+    login: "pizzadabdab",
+    password: "piZZa1234_",
+};
 
 export const SignIn = () => {
-    const { handleLogin } = useContext(AuthC);
+    const { handleSignIn, error } = useContext(AuthC);
 
-    const { handleSubmit, register } = useForm();
+    const { 
+        handleSubmit, 
+        register,
+        formState: {errors} 
+    } = useForm(
+        {
+            defaultValues,
+        }
+    );
+
+    const onSubmit = (data) => {
+        handleSignIn(data.login, data.password)
+    };
 
     return (
         <div className="login-wrapper">
-            <form onSubmit={handleSubmit(handleLogin)}>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <img src={sobaka_img} alt="Картинка собаки"/>
                 <h2>Войдите в игру</h2>
                 <div className="inputs">
-                    <input type="text" placeholder="Логин"/>
-                    <input type="password" placeholder="Пароль"/>
+                    <div className="wrap-input">
+                        <input 
+                            className={`${errors.login || error.login ? 'error' : ''}`}
+                            type="text"
+                            id="login"
+                            placeholder="Логин"
+                            {...register("login")}
+                        />
+                        {(errors?.login || error?.login) && (
+                            <p className="errors">{errors?.login?.message || error?.login}</p>
+                        )}
+                    </div>
+                    
+                    <div className="wrap-input">
+                        <input 
+                            className={`${errors.password || error.password ? 'error' : ''}`}
+                            type="password" 
+                            id="password"
+                            placeholder="Пароль"
+                            {...register("password")}
+                        />
+                        {(errors?.password || error?.password) && (
+                            <p className="errors">{errors?.password?.message || error?.password}</p>
+                        )}
+                    </div>
+                    
                 </div>
-                <button type="submit">Войти</button>
+                <button type="submit" id="submit">Войти</button>
             </form>
         </div>
     );
