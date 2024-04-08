e28a655588f58f81dcb700ee073e56e9bf07752c
neitoo <samuraa855@gmail.com>
2024-04-08 18:00:15 +0500

    Реализовано отображение сообщений чата

---
+++ src/components/chat/MessageList.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const MessageList = ({messages}) => {
+    return (
+        <div className="scroll">
+            <div className="messages-list">
+               {messages.map((message,index) =>(
+                    <div className={`wrap-message ${message.curPlayer ? "green" : "red"}`}>
+                        <div key={index} className={`message ${message.curPlayer ? "green" : "red"}`}>
+                            <div className="head-message">
+                                <p className={`name ${message.curPlayer ? "green" : "red"}`}>{message.name}</p>
+                                <span className="time">{message.time}</span>
+                            </div>
+                            <p className="message-text">{message.text}</p>
+                        </div>    
+                    </div>
+                ))} 
+            </div>
+            
+        </div>
+    );
+}
+
+export default MessageList;
