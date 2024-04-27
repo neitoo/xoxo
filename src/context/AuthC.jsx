c03cf15dfd9539e958349d15c47e15a2842945d5
neitoo <samuraa855@gmail.com>
2024-04-27 14:09:50 +0500

    Переработан код под авторизацию через апи

--- src/context/AuthC.jsx
+++ src/context/AuthC.jsx
@@ -1,6 +1,7 @@
 import { createContext, useState, useEffect } from "react";
-import { signInYup } from "./validation";
 import { ColorRing } from "react-loader-spinner";
+import $api from "../http";
+import memoryJWT from "../services/memoryJWT";
 
 export const AuthC = createContext({});
 
@@ -11,43 +12,35 @@ const AuthProvider = ({children}) => {
     const [isUserLogged, setIsUserLogged] = useState(false);
     const [error, setError] = useState({});
 
-    const testUserData = {
-        login: "pizzadabdab",
-        password: "piZZa1234_",
-        fullname: "Авторизированный игрок",
-        procentWin: 0,
-    }
+    const handleSignIn = async (login,password) => {
 
-    const handleSignIn = (login,password) => {
-        const isValidLogin = login === testUserData.login;
-        const isValidPassword = password === testUserData.password;
-        
-        const yupErrors = validateYupFields({ login, password });
-        
-        setError({
-            login: isValidLogin ? "" : "Неверный логин",
-            password: isValidPassword ? "" : "Неверный пароль",
-            ...yupErrors
-        });
-    
-        setIsUserLogged(isValidLogin && isValidPassword && Object.keys(yupErrors).length === 0);
-
+        $api.post("/auth", {login,password})
+            .then((response) => {
+                localStorage.setItem("accessToken", response.data.accessToken);
+                setIsUserLogged(true);
+                setUserData(response.data.user);
+            })
+            .catch(error => {
+                if (error.response) {
+                    const errorMessage = error.response.data.message;
+                    setError({ message: errorMessage});
+                } else {
+                    console.error("Error:", error);
+                    setError({ message: "Ошибка сервера"});
+                }
+            });
     }
 
     const handleLogOut = () => {
-        setIsUserLogged(false);
-    }
-
-    const validateYupFields = (data) => {
-        try {
-            signInYup.validateSync(data, { abortEarly: false });
-            return {}; // Если валидация прошла успешно, возвращаем пустой объект ошибок
-        } catch (error) {
-            return error.inner.reduce((acc, curr) => {
-                acc[curr.path] = curr.message; // Формируем объект ошибок
-                return acc;
-            }, {});
-        }
+        $api.post("/logout")
+            .then((response) => {
+                localStorage.removeItem("accessToken");
+                setIsUserLogged(false);
+                setUserData();
+            })
+            .catch(error => {
+                console.error("Error:", error);
+            });
     }
 
     useEffect(() => {
@@ -60,14 +53,16 @@ const AuthProvider = ({children}) => {
             setIsAppReady(true);
         }
     }, [isUserLogged]);
+    
 
     return (
         <AuthC.Provider
             value={{
                 isUserLogged,
                 handleSignIn,
-                error,
-                handleLogOut
+                handleLogOut,
+                userData,
+                error
             }}
         >
             {isAppReady ? (children) : (
