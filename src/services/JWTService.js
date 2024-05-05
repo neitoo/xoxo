d67c124a27761360e59b3e1325ab91a2a1862d1c
neitoo <samuraa855@gmail.com>
2024-05-05 16:26:26 +0500

    Добавлено сохранение id пользователя в памяти приложения

--- src/services/JWTService.js
+++ src/services/JWTService.js
@@ -3,14 +3,15 @@ import { AuthClient } from "../context/AuthC";
 const JWTService = () => {
     let inMemoryJWT = null;
     let refreshTimeout = null;
+    let inMemoryUserID = null;
 
     const refreshToken = (expiration) => {
         const timeout = expiration - 10000;
         refreshTimeout = setTimeout(() => {
             AuthClient.post("/refresh")
                 .then((res) => {
-                    const { accessToken, accessTokenExp } = res.data;
-                    setToken(accessToken, accessTokenExp);
+                    const { accessToken, accessTokenExp, id } = res.data;
+                    setToken(accessToken, accessTokenExp,id);
                 })
                 .catch(console.error);
         }, timeout);
@@ -24,17 +25,21 @@ const JWTService = () => {
 
     const getToken = () => inMemoryJWT;
 
-    const setToken = (token,tokenExpiration) => {
+    const getUserID = () => inMemoryUserID;
+
+    const setToken = (token, tokenExpiration, userId) => {
         inMemoryJWT = token;
+        inMemoryUserID = userId;
         refreshToken(tokenExpiration);
     };
 
     const deleteToken = () => {
         inMemoryJWT = null;
+        inMemoryUserID = null;
         endRefreshToken();
-    }
+    };
 
-    return { getToken, setToken, deleteToken};
+    return { getToken, getUserID, setToken, deleteToken};
 };
 
 export default JWTService();
