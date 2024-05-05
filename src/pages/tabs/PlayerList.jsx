19d6838a8027d29e36dd5d2d7ef1a7e47d965050
neitoo <samuraa855@gmail.com>
2024-05-05 19:15:24 +0500

    Добавлена начальная структура вывода всех пользователей

--- src/pages/tabs/PlayerList.jsx
+++ src/pages/tabs/PlayerList.jsx
@@ -1,10 +1,67 @@
 import React, {Component} from 'react';
-import { ClosedContent } from '../../components/ClosedContent';
+import { AuthC } from '../../context/AuthC';
+import "../../styles/MainContent.scss";
 
 export default class PlayerList extends Component{
+    static contextType = AuthC;
+
+    state = {
+        users: [],
+    };
+
+    componentDidMount = () => {
+        this.fetchUsers();
+    }
+
+    fetchUsers = () => {
+        const { handleGetUsers } = this.context;
+        handleGetUsers()
+            .then((data) => {
+                console.log(data);
+                this.setState({users: data});
+            })
+            .catch((e) => {
+                console.error(e);
+            });
+    }
+
     render(){
+        const { users } = this.state;
+
+        const headers = [
+            { label: 'ФИО', key: 'user_fullname' },
+            { label: 'Возраст', key: 'user_age' },
+            { label: 'Пол', key: 'user_gender' },
+            { label: 'Создан', key: 'registration_date' },
+        ];
+
         return(
-            <ClosedContent/>
+            <div className='wrapper'>
+                <div className="main-container">
+                    <div className="head-container">
+                        <h1>Список игроков</h1>
+                        <button>Добавить игроков</button>
+                    </div>
+                    <table>
+                        <thead>
+                            <tr>
+                            {headers.map((header) => (
+                                <th key={header.key}>{header.label}</th>
+                            ))}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {users.map((user) => (
+                                <tr key={user.id}>
+                                    {headers.map((header) => (
+                                        <td key={`${user.id}-${header.key}`}>{user[header.key]}</td>
+                                    ))}
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         )
     }
 }
