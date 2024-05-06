b970c90965a5137d795b4e1f1dd09754558823ae
neitoo <samuraa855@gmail.com>
2024-05-06 18:20:16 +0500

    Доработан вывод пользователей, реализовано разграничение по правам доступа, реализовано открытие модального окна для добавления пользователя

--- src/pages/tabs/PlayerList.jsx
+++ src/pages/tabs/PlayerList.jsx
@@ -1,65 +1,102 @@
 import React, {Component} from 'react';
 import { AuthC } from '../../context/AuthC';
 import "../../styles/MainContent.scss";
+import {formatDate} from '../../services/DateService';
+import Table from '../../components/Table';
+import close from '../../assets/close.svg';
+
 
 export default class PlayerList extends Component{
     static contextType = AuthC;
 
     state = {
         users: [],
+        userData: null,
+        hasUpdatedData: false,
+        isAdmin: false,
+        modalOpen: false,
     };
 
     componentDidMount = () => {
         this.fetchUsers();
+        
+    }
+
+    componentDidUpdate = (prevState) => {
+        const {userData} = this.context;
+        if(userData.hasOwnProperty('admin') && !this.state.hasUpdatedData){
+            this.setState({ userData });
+            this.setState({ isAdmin: userData.admin})
+            this.setState({ hasUpdatedData: true });
+        }
     }
 
     fetchUsers = () => {
         const { handleGetUsers } = this.context;
         handleGetUsers()
             .then((data) => {
-                console.log(data);
-                this.setState({users: data});
+                const formatData = data.map((user) => {
+                    const { id, user_fullname, user_age, user_gender, registration_date } = user;
+                    let gender = (user_gender === "m" ? "&#128113;" : "&#128105;");
+
+                    return {
+                        id,
+                        name: user_fullname,
+                        age: user_age,
+                        gender,
+                        registrationDate: formatDate(registration_date),
+                    };
+                })
+                this.setState({users: formatData});
             })
             .catch((e) => {
                 console.error(e);
             });
     }
 
+    handleDeleteClick = (user) => {
+        console.log("Удален юзер:", user);
+    }
+
+    handleModalOpen = () => {
+        this.setState({modalOpen: true});
+    }
+
+    handleModalClose = () => {
+        this.setState({modalOpen: false});
+    }
+
     render(){
         const { users } = this.state;
 
         const headers = [
-            { label: 'ФИО', key: 'user_fullname' },
-            { label: 'Возраст', key: 'user_age' },
-            { label: 'Пол', key: 'user_gender' },
-            { label: 'Создан', key: 'registration_date' },
+            { label: 'ФИО', key: 'name' },
+            { label: 'Возраст', key: 'age' },
+            { label: 'Пол', key: 'gender' },
+            { label: 'Создан', key: 'registrationDate' },
+            { label: '', key: 'actions' },
         ];
 
         return(
             <div className='wrapper'>
+                {this.state.modalOpen && (
+                    <div className={"modal-wrap" + (this.state.modalOpen ? "" : "closed")}>
+                        <div className="modal">
+                            <div className="close-menu">
+                                <img src={close} alt="Закрыть" onClick={this.handleModalClose}/>
+                            </div>
+                            <h1>Добавить игрока</h1>
+                        </div>
+                    </div>
+                )}
                 <div className="main-container">
                     <div className="head-container">
                         <h1>Список игроков</h1>
-                        <button>Добавить игроков</button>
+                        {this.state.isAdmin && (
+                            <button onClick={this.handleModalOpen}>Добавить игроков</button>
+                        )}
                     </div>
-                    <table>
-                        <thead>
-                            <tr>
-                            {headers.map((header) => (
-                                <th key={header.key}>{header.label}</th>
-                            ))}
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {users.map((user) => (
-                                <tr key={user.id}>
-                                    {headers.map((header) => (
-                                        <td key={`${user.id}-${header.key}`}>{user[header.key]}</td>
-                                    ))}
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                    <Table data={users} isAdmin={this.state.isAdmin} headers={headers} showActions={true} textActions='Удалить' onDataClick={this.handleDeleteClick}/>
                 </div>
             </div>
         )
